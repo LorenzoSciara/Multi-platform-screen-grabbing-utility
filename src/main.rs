d4108720e8ab7497229af9ea7aa114ac71d6ab25
FRANCESCO GRANDE s296962 <s296962@studenti.polito.it>
2023-11-15 17:18:22 +0100

    Implementazione di cattura dello schermo in formato png

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod screen_capture;
+mod image_processing;
+
+use screen_capture::ScreenCapturer;
+use image_processing::ImageProcessor;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut capturer = ScreenCapturer::new()?;
+    let (frame, width, height) = capturer.capture_screen()?;
+
+    let processor = ImageProcessor::new();
+    processor.process_and_save_image(&frame, width, height, "screenshot.png")?;
+
+    Ok(())
 }
