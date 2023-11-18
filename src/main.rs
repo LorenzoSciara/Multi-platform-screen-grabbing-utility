2ba441b7d6305c8b9b9fe65955474c612f7e62dd
LorenzoSciara <s303462@studenti.polito.it>
2023-11-18 19:35:10 +0100

    Dummy bottons added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use iced::executor;
-use iced::{Application, Command, Element, Settings, Theme};
+use iced::{Element, Sandbox, Settings, Alignment};
+use iced::widget::{button, row};
 
 pub fn main() -> iced::Result {
     Screenshot::run(Settings::default())
@@ -10,28 +10,39 @@ struct Screenshot;
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     NewScreenshot,
-
+    Settings,
 }
 
-impl Application for Screenshot {
-    type Executor = executor::Default;
-    type Flags = ();
-    type Message = ();
-    type Theme = Theme;
-
-    fn new(_flags: ()) -> (Screenshot, Command<Self::Message>) {
-        (Screenshot, Command::none())
+impl Sandbox for Screenshot {
+    type Message = Message;
+    fn new() -> Screenshot {
+        Screenshot
     }
-
     fn title(&self) -> String {
         String::from("Multi-platform Screen-grabbing Utility")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::NewScreenshot =>{
+
+            }
+            Message::Settings => {
+
+            }
+        }
     }
 
-    fn view(&self) -> Element<Self::Message> {
-        "Hello, world!".into()
+    fn view(&self) -> Element<Message> {
+        // We use a column: a simple vertical layout
+        return row![
+            // The increment button. We tell it to produce an
+            // `IncrementPressed` message when pressed
+            button("New Screenshot").on_press(Message::NewScreenshot),
+
+            // The decrement button. We tell it to produce a
+            // `DecrementPressed` message when pressed
+            button("Settings").on_press(Message::Settings),
+        ].padding(100).align_items(Alignment::Center).into();
     }
 }
