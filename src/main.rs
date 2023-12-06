25a6ad2860d2d943bb6986864321a39040b78cb3
LorenzoSciara <s303462@studenti.polito.it>
2023-12-06 18:06:25 +0100

    Iced with Application

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,34 @@ mod UI{pub mod home; pub mod modify; pub mod settings;}
 use crate::UI::home::home;
 use crate::UI::modify::modify;
 use crate::UI::settings::settings;
+use iced::{executor, Size};
+use iced::widget::{button, column, container};
+use iced::window;
+use iced::{Alignment, Application, Command, Element, Length, Settings, Theme};
+use std::thread;
+use std::time::Duration;
 
-use std::iter::Filter;
-use std::path::Path;
-use iced::{Element, Sandbox, Settings, Alignment, Length, alignment, theme, window};
-use iced::Background::Color;
-use iced::widget::{button, row, text, column, image, Image, container};
+pub fn main() /*-> iced::Result*/ { //Il main non ritorna per permettere la programmazione multithread
+    let handle = thread::spawn(|| {
+        // Il thread dorme per 10 secondi
+        thread::sleep(Duration::from_secs(10));
+        println!("Il thread è stato in pausa per 10 secondi.");
+    });
 
-
-struct Screenshot {
-    name: String,
-    pageState: PagesState,
-    screenState: ScreenState,
-    image: image::Handle,
+    let settings = Settings {
+        window: window::Settings {
+            size: (350, 80), // Imposta le dimensioni della finestra
+            ..Default::default()
+        },
+        ..Settings::default()
+    };
+    Screenshot::run(settings);
+    handle.join().unwrap();
 }
 
-#[derive(Debug, Clone)]
-pub enum Message{
-    PagesState(PagesState),
-    ScreenState(ScreenState),
+struct Screenshot {
+    pageState: PagesState,
+    screenState: ScreenState,
 }
 
 #[derive(
@@ -40,49 +49,46 @@ pub enum ScreenState{
     ScreenFalse,
 }
 
-
-pub fn main() -> iced::Result {
-    let settings = Settings {
-        window: window::Settings {
-            size: (350, 80), // Imposta le dimensioni della finestra
-            ..Default::default()
-        },
-        ..Settings::default()
-    };
-    Screenshot::run(settings)
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    PagesState(PagesState),
+    ScreenState(ScreenState),
 }
 
-impl Sandbox for Screenshot {
+impl Application for Screenshot {
+    type Executor = executor::Default;
     type Message = Message;
-    fn new() -> Screenshot {
-        Screenshot {
-            name: "Empty".to_string(),
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        return (Screenshot {
             pageState: PagesState::Home,
             screenState: ScreenState::ScreenFalse,
-            image: image::Handle::from_path(Path::new("./resources/empty-image.png")),
-        }
+        }, Command::none());
     }
 
     fn title(&self) -> String {
-        String::from("Multi-platform Screen-grabbing Utility")
+        String::from("Exit - Iced")
     }
 
-    fn update(&mut self, message: Self::Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::PagesState(filter) => {
-                    self.pageState=filter;
+                self.pageState=filter;
+                return Command::none();
             },
             Message::ScreenState(filter) => {
                 self.screenState=filter;
+                return window::minimize(true);
             }
         }
     }
 
-    fn view(&self) -> Element<'static, Message> {
-        // We use a column: a simple vertical layout
+    fn view(&self) -> Element<Message> {
         return container(
             match self.pageState {
-                PagesState::Home=> match self.screenState {
+                PagesState::Home => match self.screenState {
                     ScreenState::ScreenTrue => home(ScreenState::ScreenTrue),
                     ScreenState::ScreenFalse => home(ScreenState::ScreenFalse),
                 },
