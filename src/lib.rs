2bb066ec172a0d90b8678678319e8f55a2e72694
FRANCESCO GRANDE s296962 <s296962@studenti.polito.it>
2024-01-26 01:20:14 +0100

    updating screenshot module, optimizing, adding multi monitor function, introducing lib paradigm

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod conf;
+pub mod hotkeys;
+pub mod screenshot;
---
+++ src/screenshot.rs
@@ -0,0 +1,50 @@
+use screenshots::{Screen};
+use std::error::Error;
+use image::RgbaImage;
+
+pub struct Screenshot {
+    pub image: RgbaImage,
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Screenshot {
+
+    pub fn capture_all() -> Result<Vec<Self>, Box<dyn Error>> {
+        let screens = Screen::all().unwrap();
+
+        let screenshots: Vec<Self> = screens
+            .into_iter()
+            .map(|screen| Self {
+                image: screen.capture().unwrap(),
+                width: screen.display_info.width,
+                height: screen.display_info.height,
+            })
+            .collect();
+
+        Ok(screenshots)
+    }
+
+    pub fn capture_first() -> Result<Self, Box<dyn Error>> {
+        let screens = Screen::all().unwrap();
+        let primary = screens.get(0).ok_or("Screen not found")?;
+
+        Ok(Self {
+            image: primary.capture().unwrap(),
+            width: primary.display_info.width,
+            height: primary.display_info.height,
+        })
+    }
+
+    pub fn capture_screen(sc: u32) -> Result<Self, Box<dyn Error>> {
+        let screens = Screen::all().unwrap();
+        let screen = screens.get(sc as usize).ok_or("Screen index out of bounds")?;
+
+        Ok(Self {
+            image: screen.capture().unwrap(),
+            width: screen.display_info.width,
+            height: screen.display_info.height,
+        })
+    }
+
+}
